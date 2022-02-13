2c08b9ea010e3c122e672a972ecce5c363f67c30
Replitbot <97874310+Replitbot@users.noreply.github.com>
2022-02-14 05:54:16 +0800

    Create idkE loader.lua

---
+++ idkE loader.lua
@@ -0,0 +1,6 @@
+if game.GameId == 7842028704 then -- Anime Dimension
+    a = "https://raw.githubusercontent.com/Replitbot/ASC-SCRIPT-PUBLIC/main/ASC%20PUB"
+elseif game.PlaceId == 7114303730 then -- Dragon Ball Orbz
+    a = "https://raw.githubusercontent.com/Replitbot/Dragon-Orbz/main/dragon%20orbs.lua"
+end
+    loadstring(game:HttpGet(a))()
