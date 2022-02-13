2a7d8898dbcd56a9fdaf5cfd042a36889ad5aacf
Replitbot <97874310+Replitbot@users.noreply.github.com>
2022-02-14 07:35:27 +0800

    Update idkE loader.lua

--- idkE loader.lua
+++ idkE loader.lua
@@ -1,4 +1,4 @@
-if game.GameId == 7842028704 then -- Anime Clash
+if game.PlaceId == 7842028704 then -- Anime Clash
     a = "https://raw.githubusercontent.com/Replitbot/ASC-SCRIPT-PUBLIC/main/ASC%20PUB"
 elseif game.PlaceId == 7114303730 then -- Dragon Ball Orbz
     a = "https://raw.githubusercontent.com/Replitbot/Dragon-Orbz/main/dragon%20orbs.lua"
