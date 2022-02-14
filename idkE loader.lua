51c189859bd84379fe8a88091728cb90e3a8c5fb
Replitbot <97874310+Replitbot@users.noreply.github.com>
2022-02-14 09:00:18 +0800

    Update idkE loader.lua

--- idkE loader.lua
+++ idkE loader.lua
@@ -1,6 +1,8 @@
 if game.PlaceId == 7842028704 then -- Anime Clash
-    a = "https://raw.githubusercontent.com/Replitbot/ASC-SCRIPT-PUBLIC/main/ASC%20PUB"
+    a = "https://raw.githubusercontent.com/Replitbot/Loader-For-All/All-Games-Paid-And-Free/ACS%20FREE.lua"
 elseif game.PlaceId == 7114303730 then -- Dragon Ball Orbz
-    a = "https://raw.githubusercontent.com/Replitbot/Dragon-Orbz/main/dragon%20orbs.lua"
+    a = "https://raw.githubusercontent.com/Replitbot/Loader-For-All/All-Games-Paid-And-Free/Dragon%20Orbz%20balls.lua"
+elseif game.PlaceId == 8448735476 then -- DBZ Adventures
+    a = "https://raw.githubusercontent.com/Replitbot/Loader-For-All/All-Games-Paid-And-Free/dbz%20adventures.lua"
 end
     loadstring(game:HttpGet(a))()
